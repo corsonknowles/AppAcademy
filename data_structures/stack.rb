a22da916535ffe9d30b82dc8aae6ea3676b6b038
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-06-03 03:48:04 -0700

    Push actual files

--- data_structures/stack.rb
+++ data_structures/stack.rb
@@ -1,17 +1,25 @@
 class Stack
   def initialize
-    # create ivar to store stack here!
+    @stack = []
   end
 
   def add(el)
     # adds an element to the stack
+    @stack.push(el)
+    @stack.dup
   end
 
   def remove
     # removes one element from the stack
+    @stack.pop
   end
 
   def show
+    @stack.deepdup
     # return a copy of the stack
+  end
+
+  def deepdup(array)
+    array.reduce([]) { |accum, e| e.is_a?(Array) ? accum << deepdup(e) : accum << e}
   end
 end
